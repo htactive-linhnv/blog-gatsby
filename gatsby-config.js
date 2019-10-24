216e130532365d24bf8f5291f07fc4b3dc00d6a1
linhnuyen <linhnv@htactive.com>
2019-10-24 14:05:41 +0700

    netlifyCMS-updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
-  plugins: [`gatsby-plugin-netlify-cms`],
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-netlify-cms`,
     {
       resolve: 'gatsby-plugin-antd',
       options: {
